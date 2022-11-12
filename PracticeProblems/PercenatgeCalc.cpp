64c76bdd2a33594834afa864c743234dcc982a35
ADRian123L <adrian163@usf.edu>
2022-11-12 15:57:36 -0500

    Added some variables.

--- PracticeProblems/PercenatgeCalc.cpp
+++ PracticeProblems/PercenatgeCalc.cpp
@@ -5,6 +5,14 @@
 
 int main(){
     
+    // Variables to hold the pay:
+    double monthPay = 7800.00, contributing;
+
+    contributing = monthPay * 0.05;
+    std::cout << "\n" << contributing << "\n";
+
     
+
+
     return 0;
 }
