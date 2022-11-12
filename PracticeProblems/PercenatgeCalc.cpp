d2e0b11a727a2ee931f253f03ac207d6deee926e
ADRian123L <adrian163@usf.edu>
2022-11-12 16:00:17 -0500

    Added a calculator for seven percent.

--- PracticeProblems/PercenatgeCalc.cpp
+++ PracticeProblems/PercenatgeCalc.cpp
@@ -8,11 +8,13 @@ int main(){
     // Variables to hold the pay:
     double monthPay = 7800.00, contributing;
 
+    // Calculates the monthly contribution at five percent:
     contributing = monthPay * 0.05;
     std::cout << "\n" << contributing << "\n";
 
-    
-
+    // Calculates the monthly contribution at seven percent:
+    contributing = monthPay * 0.07;
+    std::cout << "\n" << contributing << "\n";
 
     return 0;
 }
