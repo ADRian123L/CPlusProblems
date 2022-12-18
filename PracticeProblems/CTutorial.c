6201ef3d1256828f9b39935736acd82542cac404
ADRian123L <adrian163@usf.edu>
2022-12-18 17:08:59 -0500

    Added a rounder to the place holder.

--- PracticeProblems/CTutorial.c
+++ PracticeProblems/CTutorial.c
@@ -11,89 +11,11 @@ int number = 78;
 float decimal[] = {67.9, 897, 876, 67};
 char name = 'L';
 char place[] = "USF";
-
+double big_num = 9799.098778990;
 // functions:
-
-void out1();
-int min(int first, int second, int third);
-void out(int biggest);
-int prompt(void);
-int max(int nums[3]);
 
 int main(){
 
-
-    // Calls the functions:
-    int number = prompt();
-    int calculate = max(&number);
-    out(calculate);
-
+    printf("%.2f\n", big_num);
     return 0;
-}
-
-void out1(void){
-
-    printf("It works!!!\n");
-
-}
-
-int min(int first, int second, int third){
-    int smallest;
-    if (first < second && first < third){
-        smallest = first;
-    }
-    else if (second < third && second < first){
-        smallest = second;
-    }
-    else {
-        smallest = third;
-    }
-
-    return smallest;
-}
-
-int prompt(void){
-
-    // Empty array:
-    int elements[3];
-
-    // Prompts for three numbers:
-    scanf("%d%d%d", &elements[0], &elements[1], &elements[2]);
-
-    return elements;
-}
-
-int max(void){
-    
-    // Empty array:
-    int elements[3];
-
-    // Prompts for three numbers:
-    scanf("%d%d%d", &elements[0], &elements[1], &elements[2]);
-
-    // Variables:
-    const int ONE = elements[0], 
-              TWO = elements[1], 
-              THREE = elements[2];
-    
-    int largest_num = 0;
-
-    // Compare:
-    if (ONE > TWO && ONE > THREE){
-        largest_num = ONE;
-    }
-
-    else if (TWO > THREE && TWO > ONE){
-        largest_num = TWO;
-    }
-
-    else {
-        largest_num = THREE;
-    }
-
-    return largest_num;    
-}
-
-void out(int biggest_number) {
-    printf("%d\n", biggest_number);
 }
