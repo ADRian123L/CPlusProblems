0a49f40dd9872660d46d8681c598015758d8bae3
ADRian123L <adrian163@usf.edu>
2023-03-10 15:15:08 -0500

    Setup a loop to search through the columns of ary.

--- ProgramDesign/MultiDimenArray.c
+++ ProgramDesign/MultiDimenArray.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define ROW 5
+#define ROW 4
 #define COL 3
 
 int main(void) 
@@ -8,15 +8,15 @@ int main(void)
     // Array:
     int array[ROW][COL] = {{1, 2, 3}, 
                            {4, 5, 6}, 
-                           {7, 8, 9}};
+                           {7, 8, 9},
+                           {10, 11, 12}};
 
-    int *p = &array[0][0];
+    int (*p)[COL] = &array[0];
     int index;
 
-    for (; p < &array[ROW][COL]; ++p)
+    for (; p < &array[ROW - 1]; ++p)
     {
-        index = p - &array[0][0];
-        printf("The value of %d is: %d\n", index, *p);
+        printf("The first and second elements of the array row %d are: %d and %d.\n", (int) (p - &array[0]), **p, *(*p + 1));
     }
 
     return 0;
