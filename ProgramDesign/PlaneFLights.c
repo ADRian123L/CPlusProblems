44397446fe344fa11e4e175f928945940fa0a73d
ADRian123L <adrian163@usf.edu>
2023-03-10 17:11:47 -0500

    Completed the plane problem.

--- ProgramDesign/PlaneFLights.c
+++ ProgramDesign/PlaneFLights.c
@@ -43,7 +43,9 @@ int main(void)
     arrive[0] = (int) arrival / 100;
     arrive[1] = (int ) arrival % 100;
 
-    printf("The closet departure time is %d:%d %s, arriving at %d:%d %s.\n", depart[0], depart[1], j, arrive[0], arrive[1], u);
+    // Output the result:
+    printf("The closet departure time is %d:%d %s, arriving at %d:%d %s.\n", 
+            depart[0], depart[1], j, arrive[0], arrive[1], u);
     
     return 0;
 }
@@ -52,15 +54,15 @@ int main(void)
 void find_closest_flight(int desired_time, int *departure_time, int *arrival_time)
 {
     // Flight schedule:
-    int time[ROW][COL] = {{800, 1016}, 
-                        {943, 1152}, 
-                        {1119, 1331}, 
-                        {1247, 1500}, 
-                        {1400, 1608}, 
-                        {1548, 1755}, 
-                        {1900, 2120}, 
-                        {2145, 2358}, 
-                                   0};
+    int time[ROW][COL] = {{800, 1016},  // 0
+                        {943, 1152},    // 1
+                        {1119, 1331},   // 2
+                        {1247, 1500},   // 3
+                        {1400, 1608},   // 4
+                        {1548, 1755},   // 5
+                        {1900, 2120},   // 6
+                        {2145, 2358},   // 7
+                                   0};  // 8
     
     // Save the indexes of the flights:
     int indexes[2] = {0},
@@ -69,31 +71,30 @@ void find_closest_flight(int desired_time, int *departure_time, int *arrival_tim
     // Locate the closest departure time:
     for (int (*p)[COL] = &time[0]; p < &time[ROW - 1]; ++p)
         if ((desired_time) >= **p && (desired_time) <= *(*p + 1))
-            *j++ = p - &time[0] + 1;
+            *j++ = (p - &time[0]) * 100;
     
-    // Initializing variables for string the results:
+    // Initializing variables for storing the results:
     int diff_1, 
         diff_2, 
         *k = indexes;
-
-    printf("The values of indexes are: %d and %d.\n", indexes[0], indexes[1]);
-    
-    // Finding which is the closest departure time:
-    diff_1 = time[*k - 1][1] - desired_time;
-    diff_2 = time[*(++k) - 1][1] - desired_time;
-
-    // The absolute value:
-    if (diff_1 < 0)
-        diff_1 *= -1;
-    if (diff_2 < 0)
-        diff_2 *= -1;
-    
     int element;
-    if (diff_1 > diff_2)
-        element = indexes[0];
+
+    // Find which is the closest index:
+    if (indexes[0] == 0)
+        element = indexes[1] / 100;
+    else if (indexes[1] == 0)
+        element = indexes[0] / 100;
     else
-        element = indexes[1];
+        {
+            int temp1, temp2, temp3, temp4;
+            temp1 = time[indexes[0] / 100][1] - desired_time;
+            temp2 = time[indexes[1] / 100][1] - desired_time;
+            temp3 = (temp1 * temp1);
+            temp4 = (temp2 * temp2); 
+            element = (temp3 > temp4) ? (indexes[1] / 100) : (indexes[0] / 100);
+        }
     
+    // Return the values:
     *departure_time = time[element][0];
     *arrival_time = time[element][1];
 }
