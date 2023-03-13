058c7ac332e6fe4406cd546d51b686992235f3f7
ADRian123L <adrian163@usf.edu>
2023-03-13 15:06:33 -0400

    Made the algorithm faster.

--- ProgramDesign/LeetCode.c
+++ ProgramDesign/LeetCode.c
@@ -11,46 +11,47 @@ int main(void)
 // The function to find the pivot index:
 int pivotIndex(int* nums, int numsSize)
 {
+    // Check if the array length if less than 1:
+    if (numsSize < 1)
+	return -1;
+
     // Initializing some variables to store the data:
-    int sumr = 0, suml = 0, flag = 0;
+    int flag = 0, sum = 0;
     int *p, *q;
+ 
+    // Get the total sum:
+    for (p = &nums[0]; p < &nums[numsSize]; ++p)
+    	sum += *p;
 
     // Setting q and p to point to nums:
-    p = q = &nums[0];
+    p = &nums[0];
 
     // Search for the pivot:
-    for (; p < &nums[numsSize]; ++p)
+    for (float suml; p < &nums[numsSize]; ++p)
     {   
+        // Check if the suml is an int:
+        suml = ((sum - *p) / 2.0f);
+	if ((suml - (int) suml) != 0.0f)
+		continue;
+
         // Start q at the first element:
         q = &nums[0];
-        suml = 0;
-        sumr = 0;
+        suml = 0.0f;
 
         // Find the sum of the left-hand side values:
-        if (q == p)
-            suml += 0;
         while(q < p)
         {
             // Take the sum of the elements:
             suml += *q;
             ++q;
         }
+	
+	    if (suml == ((sum - *p) / 2.0f))
+	    {
+	        flag = 1;
+	        break;
+	    }
 
-        // Fing the sum of the right-hand side:
-        if (q == p)
-            ++q;
-        while (q > p && q < &nums[numsSize])
-        {
-            sumr += *q;
-            ++q;
-        }
-
-        // Check if the sums are eqaul:
-        if (sumr == suml)
-        {   
-            flag = 1;
-            break;
-        }
     }
   
     // Check if there is a pivot:
