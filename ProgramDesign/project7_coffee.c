e4328f5bf5ded39776156e105e18077fbef83936
ADRian123L <adrian163@usf.edu>
2023-03-28 23:22:47 -0400

    Created the file for project 7

---
+++ ProgramDesign/project7_coffee.c
@@ -0,0 +1,13 @@
+/*
+    Purpose: The program reads a cvs file. Stores the contents of the file.
+	     And sorts the content of the file by the score on assigned to 
+             each coffee type. Afterwards, the program writes the sorted 
+     	     information into a file.
+    Author: Adrian Lozada
+*/
+#include <stdio.h>
+
+int main(void)
+{
+
+}
