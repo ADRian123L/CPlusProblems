3c670a76bb10399fe8a9a1e64f6dcd8961c5f64a
ADRian123L <adrian163@usf.edu>
2023-04-10 22:41:47 -0400

    Adding project 8.

---
+++ ProgramDesign/project8_scheduler.c
@@ -0,0 +1,218 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+//////////////////////
+// data definitions //
+//////////////////////
+
+#define NAME_LEN 20
+#define SIZE_LEN 3
+
+struct job {
+	char job_name[NAME_LEN+1], user_name[NAME_LEN+1];
+	int num_cpus, num_gpus, memory, priority;
+	double time;
+	struct job *next;
+};
+
+/////////////////////////
+// function prototypes //
+/////////////////////////
+
+void help();
+void read(char *job_name, char *user_name, int *num_cpus, int *num_gpus, int *memory, double *time, int *priority);
+struct job * add_job(struct job *scheduler, char *job_name, char *user_name, int num_cpus, int num_gpus, int memory, double time, int priority);
+struct job * pop_job(struct job *scheduler);
+void list_user(struct job *scheduler, char *user_name);
+void list_jobs(struct job *scheduler, int num_cpus, int num_gpus, int memory, double time);
+void list_all_jobs(struct job *scheduler);
+struct job * clear_jobs(struct job *scheduler);
+
+///////////////////
+// main function //
+///////////////////
+
+int main() {
+	char code;
+	char job_name[NAME_LEN+1], user_name[NAME_LEN+1];
+	int num_cpus, num_gpus, memory, priority;
+	double time;
+
+	struct job *scheduler = NULL;
+
+	help();
+	printf("\n");
+
+	for (;;) {
+		// read operation code
+		printf("Enter operation code: ");
+		scanf(" %c", &code);
+		while(getchar() != '\n') /* skips to end of line */
+			;
+
+		// run operation
+		switch (code) {
+			case 'h':
+				help();
+				break;
+			case 'a':
+				read(job_name, user_name, &num_cpus, &num_gpus, &memory, &time, &priority);
+				scheduler = add_job(scheduler, job_name, user_name, num_cpus, num_gpus, memory, time, priority);
+				break;
+			case 'p':
+				scheduler = pop_job(scheduler);
+				break;
+			case 'u':
+				read(NULL, user_name, NULL, NULL, NULL, NULL, NULL);
+				list_user(scheduler, user_name);
+				break;
+			case 'j':
+				read(NULL, NULL, &num_cpus, &num_gpus, &memory, &time, NULL);
+				list_jobs(scheduler, num_cpus, num_gpus, memory, time);
+				break;
+			case 'l':
+				list_all_jobs(scheduler);
+				break;
+			case 'q':
+				scheduler = clear_jobs(scheduler);
+				return 0;
+			default:
+				printf("Illegal operation code!\n");
+		}
+		printf("\n");
+	}
+}
+
+//////////////////////////
+// function definitions //
+//////////////////////////
+
+void help() {
+	printf("List of operation codes:\n");
+	printf("\t'h' for help;\n");
+	printf("\t'a' for adding a job to the scheduler;\n");
+	printf("\t'p' for removing a job from the scheduler;\n");
+	printf("\t'u' for searching jobs per user;\n");
+	printf("\t'j' for searching jobs per capacity;\n");
+	printf("\t'l' for listing all jobs;\n");
+	printf("\t'q' for quit.\n");
+ } void read(char *job_name, char *user_name, int *num_cpus, int *num_gpus, int *memory, double *time, int *priority) { if(job_name != NULL) { printf("Enter the name of the job: "); scanf("%s", job_name); } if(user_name != NULL) { printf("Enter the name of the user: "); scanf("%s", user_name); } if(num_cpus != NULL) { printf("Enter the number of CPUs: "); scanf("%d", num_cpus); } if(num_gpus != NULL) { printf("Enter the number of GPUs: "); scanf("%d", num_gpus);
+	}
+	if(memory != NULL) {
+		printf("Enter the amount of memory: ");
+		scanf("%d", memory);
+	}
+	if(time != NULL) {
+		printf("Enter the amount of time: ");
+		scanf("%lf", time);
+	}
+	if(priority != NULL) {
+		printf("Enter the priority: ");
+		scanf("%d", priority);
+	}
+}
+
+/////////////////////////////////////////////////////////
+// WARNING - WARNING - WARNING - WARNING - WARNING     //
+/////////////////////////////////////////////////////////
+// Do not modify anything before this point, otherwise //
+// your solution will be considered incorrect.         //
+/////////////////////////////////////////////////////////
+
+struct job * add_job(struct job *scheduler, char *job_name, char *user_name, int num_cpus, int num_gpus, int memory, double time, int priority) {
+
+	// Creates a new node:
+	struct job *new_jop = (struct *job) malloc(sizeof(struct job));
+	if (new_job == NULL)
+	{
+		printf("Error: Malloc could not allocate memory");
+		return scheduler:
+	}
+	
+	// Locate the final node in the linked list:
+	struct job *tail = scheduler;
+	for (struct *head = (scheduler -> next); (head != NULL); head = head -> next, tail = tail -> next)
+		;
+
+	// Change the address stored in next of the last node:
+        tail -> next = new_job;
+	
+	// Make the next last node point to NULL:
+        new_job -> next = NULL;
+
+	// Add all of the information provided to the node:
+	strcpy(new_job -> job_name, job_name);
+	strcpy(new_job -> user_name, user_name);
+	new_job -> num_cpus = num_cpus;
+	new_job -> num_gpus = num_gpus;
+	new_job -> memory = memory;
+	new_job -> time = time;
+	new_job -> priority = priority; 
+		
+}
+
+struct job * pop_job(struct job *scheduler) {
+
+	// add code
+
+	// output format
+	printf("|----------------------|----------------------|------|------|------|--------|----------|\n");
+	printf("| Job name             | User name            | CPUs | GPUs | Mem. | Time   | Priority |\n");
+	printf("|----------------------|----------------------|------|------|------|--------|----------|\n");
+	printf("| %-20s | %-20s | %4d | %4d | %4d | %6.2f | %8d |\n", "", "", 0, 0, 0, 0, 0);
+	printf("|----------------------|----------------------|------|------|------|--------|----------|\n");
+}
+
+void list_user(struct job *scheduler, char *user_name) {
+
+	// add code
+
+	// output format
+	printf("|----------------------|----------------------|------|------|------|--------|----------|\n");
+	printf("| Job name             | User name            | CPUs | GPUs | Mem. | Time   | Priority |\n");
+	printf("|----------------------|----------------------|------|------|------|--------|----------|\n");
+	printf("| %-20s | %-20s | %4d | %4d | %4d | %6.2f | %8d |\n", "", "", 0, 0, 0, 0, 0);
+	printf("|----------------------|----------------------|------|------|------|--------|----------|\n");
+	printf("| %-20s | %-20s | %4d | %4d | %4d | %6.2f | %8d |\n", "", "", 0, 0, 0, 0, 0);
+	printf("|----------------------|----------------------|------|------|------|--------|----------|\n");
+	// ...
+}
+
+void list_jobs(struct job *scheduler, int num_cpus, int num_gpus, int memory, double time) {
+
+	// add code
+
+	// output format
+	printf("|----------------------|----------------------|------|------|------|--------|----------|\n");
+	printf("| Job name             | User name            | CPUs | GPUs | Mem. | Time   | Priority |\n");
+	printf("|----------------------|----------------------|------|------|------|--------|----------|\n");
+	printf("| %-20s | %-20s | %4d | %4d | %4d | %6.2f | %8d |\n", "", "", 0, 0, 0, 0, 0);
+	printf("|----------------------|----------------------|------|------|------|--------|----------|\n");
+	printf("| %-20s | %-20s | %4d | %4d | %4d | %6.2f | %8d |\n", "", "", 0, 0, 0, 0, 0);
+	printf("|----------------------|----------------------|------|------|------|--------|----------|\n");
+	// ...
+}
+
+void list_all_jobs(struct job *scheduler) {
+
+	// add code
+
+	// output format
+	printf("|----------------------|----------------------|------|------|------|--------|----------|\n");
+	printf("| Job name             | User name            | CPUs | GPUs | Mem. | Time   | Priority |\n");
+	printf("|----------------------|----------------------|------|------|------|--------|----------|\n");
+	printf("| %-20s | %-20s | %4d | %4d | %4d | %6.2f | %8d |\n", "", "", 0, 0, 0, 0, 0);
+	printf("|----------------------|----------------------|------|------|------|--------|----------|\n");
+	printf("| %-20s | %-20s | %4d | %4d | %4d | %6.2f | %8d |\n", "", "", 0, 0, 0, 0, 0);
+	printf("|----------------------|----------------------|------|------|------|--------|----------|\n");
+	// ...
+}
+
+struct job * clear_jobs(struct job *scheduler) {
+
+	// add code
+
+}
+
