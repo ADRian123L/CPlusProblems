1f6914e12f89c8563bd4340f4840d40bb2d23469
ADRian123L <adrian163@usf.edu>
2023-04-18 16:42:25 -0400

    Added a header file the function prot.

--- ProgramDesign/linked_list.c
+++ ProgramDesign/linked_list.c
@@ -1,46 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-struct node *add_to_list(struct node *list, int n);
-struct node *search_list(struct node *list, int n);
-struct node *delete_from_list(struct node *list, int n);
-void print_list(struct node *list);
-int duplicates(struct node *list);
-void clear_list(struct node *list);
-struct node *find_largest(struct node *list);
-
-struct node {
-	int value;          /* data stored in the node  */
-	struct node *next;  /* pointer to the next node */
-};
-
-int main()
-{
-
- 	struct node *first = NULL;
-
-	/* inserting nodes */
-	first = add_to_list(first, 3);
-	first = add_to_list(first, 64);
-	first = add_to_list(first, 98);
-	first = add_to_list(first, 3);
-        first = add_to_list(first, 64);
-	first = add_to_list(first, 3);
-	first = add_to_list(first, 136);
-
-	print_list(first);
-	
-	//add code here
-	struct node *p;
-	p = find_largest(first);
-	printf("The address of the largest value: %p\n", p);
-	printf("And the value is: %d\n", p -> value);
- 	
-	
-	clear_list(first);
-	return 0;
-
-}
+#include "linked_list.h"
 
 struct node *add_to_list(struct node *list, int n)
 {
@@ -66,6 +24,24 @@ struct node *search_list(struct node *list, int n)
 	  return NULL;
 }
 
+struct node *delete_from_list(struct node *list, int n)
+{
+  struct node *cur, *prev;
+	  
+  for (cur = list, prev = NULL;
+       cur != NULL && cur->value != n;
+       prev = cur, cur = cur->next)
+    ;
+  if (cur == NULL) 
+    return list;             /* n was not found */
+  if (prev == NULL)
+    list = list->next;       /* n is in the first node */
+  else
+    prev->next = cur->next;  /* n is in some other node */
+  free(cur);
+  return list;
+}
+
 void print_list(struct node *list)
 {
 	struct node *p;
@@ -74,37 +50,3 @@ void print_list(struct node *list)
 	printf("\n");
 }
 
-
-void clear_list(struct node *list)
-{
-  struct node *p;
-  while(list != NULL)
-  {
-	 p = list;
-     list = list->next;
-     if( p!= NULL)
-           free(p);
-  }
-}
-
-struct node *find_largest(struct node *list)
-{
-	if (list == NULL)
-		return NULL;
-
-	struct node *p = list;
-	struct node *largest = list;
-	// Locate the largest:
-	for (; p != NULL; p = p -> next)
-		if (p -> value > largest -> value)
-			largest = p;
-
-	return largest;
-}
-
-int duplicates(struct node *list){
-       //add code here
-
-       return 0;
-
-}
