82f022672e5894184ba3e058df59165cacbfa697
ADRian123L <adrian163@usf.edu>
2023-05-22 23:33:12 -0400

    Added partial solution

--- PracticeProblems/AddTwoNumbers.cpp
+++ PracticeProblems/AddTwoNumbers.cpp
@@ -18,91 +18,94 @@ int main(void)
 struct ListNode {
 	int val;
 	ListNode *next;
-	ListNode() : val(0), next(nullptr) {}
-	ListNode(int x) : val(x), next(nullptr) {}
-	ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
+// Second solution:
+
 class Solution {
 public:
+	void AddList (ListNode*& l1, ListNode*& l2, ListNode*& head);
+	ListNode* Next (ListNode* list);
+	int Val (ListNode* list);	
+	bool Mem (ListNode* l1, ListNode* l2);
 
-	
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-	int L1 {getInts(l1)}, L2 {getInts(l2)};	
-	int sum {L1 + L2};
-	
-	ListNode *head;
-	Solution::void setLink (head, sum);	
-	return head; 
-    
+	ListNode* head = NULL;
+	AddList (l1, l2, head);
+	return head;	
     }
 };
 
-Solution::int getLength(ListNode* head)
+void Solution::AddList (ListNode*& l1, ListNode*& l2, ListNode*& head)
 {
-	int length {0};
-	for (; head != NULL; head = head -> next, ++length)
-		;
-	return length;
-}
-
-Solution::void copyInt (ListNode* head, int* array)
-{
-	for (; head != NULL; head = head -> next)
-		array++ = head -> val;
-}
-
-Solution::int powten (int expo)
-{
-	if (expo == 0)
-		return 1;
-	int result = 1;
-	for (int i = 0; i < expo; ++i)
-		result *= 10;
-	return result;
-}
-Solution::int getInt (ListNode* head, int &number)
-{
-	int length = Solution::getLength (head);
-	int array[length] {0};	
-	Solution::copyInt (head, array);
-	number = 0;
-	for (int i = 0; i < length; ++i)
-		number += array[i] * Solution::powten(i);	
-	return length;
-}
-// Second solution:
-
-Solution::int getInts (ListNode* head)	
-{
-	int number {0};
-	for (int i = 1; head != NULL; head = head -> next, i *= 10)
-		number += (head -> val) * i;
-		
-	return number;
-}
-
-Solution::int numsInt (int num)
-{
-	if (num < 10)
-		return 1;
-
-	int length {0};
-	for (; num > 0; ++length) 
-		num =/ 10;
-
-	return length;	
-}
-
-Solution::void setLink (ListNode* head, int& num)
-{
-	int length = Solution::numsInt (num);
-	head = new ListNode[length];
-	ListNode *p = head, *q {NULL};
-	for (int i {10}, j {1}, count {0}; q = p count < length; ++count, i *= 10, j *= 10, p = p -> new)
+	// Allocate mem for the first node:
+	head = new ListNode;
+	ListNode *p = head, *q = head;
+	
+	int carryOut = 0, tmp;
+	for (; !(l1 == NULL && l2 == NULL); l1 = Next(l1), l2 = Next(l2), q = p)
 	{
-		p -> val = static_cast<int> (num % i) / j; 
-		if (q != NULL)
-			q -> next = p;	
+		if (carryOut == 1)
+			tmp = Val(l1) + Val(l2) + 1;
+		else
+			tmp = Val(l1) + Val(l2); 
+		// Check if there is a carry bit:
+		if (tmp > 9)
+		{
+			carryOut = 1;
+			tmp = tmp % 10;
+		}
+		else
+			carryOut = 0;
+		
+		// Assign the value to head:
+		p -> val = tmp;	
+		// Allocate more mem is neccesarry:
+		if (Mem(l1, l2))
+			p = new ListNode;
+		// Point q to p:
+		q -> next = p;
 	}
-	p -> next = NULL;			
+	p -> next = NULL;
 }
+
+bool Solution::Mem (ListNode* l1, ListNode* l2)
+{
+	if (l1 == NULL || l2 == NULL)
+	{
+		if (l1 == NULL)
+		{
+			if (l2 -> next == NULL)
+				return false;
+			else 
+				return true;
+		}
+		else if (l2 == NULL)
+		{
+			if (l1 -> next == NULL)
+				return false;
+			else
+				return true;
+		}
+	}
+	if (l1 -> next == NULL && l2 -> next == NULL)
+		return false;
+	return false;	
+}
+
+ListNode* Solution::Next (ListNode* list)
+{
+	if (list == NULL)
+		return NULL;
+	if (list -> next == NULL)
+		return NULL;
+	else
+		return list -> next;
+}
+
+int Solution::Val (ListNode* list)
+{
+	if (list == NULL)
+		return 0;
+	else
+		return list -> val;
+}	
