1ca582b37cdbe7423a2dd36184a874fd21f7e880
Adrian <99932234+ADRian123L@users.noreply.github.com>
2023-05-29 22:09:34 -0400

    Update refernces.cpp
    Added a descriptive comment on references.

--- DataStructures/refernces.cpp
+++ DataStructures/refernces.cpp
@@ -6,6 +6,9 @@ int main(void)
 	int a {10};
 	// Declaring and Initializing reference varible r.
 	int &r = a;
+    // The cariable 'r' is an alias to variable a. 
+    // Therefore, they share the same value and if
+    // changwe the other will also be changed.
 	std::cout << "The value in a: " << a << ". The value in the reference value r: " << r << std::endl;
 	return 0;
 }
